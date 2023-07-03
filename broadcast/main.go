0e5f0f6a8af465762c107c8a743c894177a79c4b
Mateo Bošnjak <bosnjakmateo1@gmail.com>
2023-07-03 21:36:27 +0200

    Challenge #3b: Multi-Node Broadcast

--- broadcast/main.go
+++ broadcast/main.go
@@ -4,18 +4,22 @@ import (
 	"encoding/json"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 	"log"
+	"strings"
 )
 
 type Json map[string]any
 
 type Body struct {
-	Message int `json:"message"`
+	Message  int                 `json:"message"`
+	Topology map[string][]string `json:"topology"`
 }
 
 func main() {
 	n := maelstrom.NewNode()
 
 	messages := make([]int, 0)
+	messagesMap := make(map[int]bool)
+	topology := make(map[string][]string)
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var body Body
@@ -23,7 +27,25 @@ func main() {
 			return err
 		}
 
+		if messagesMap[body.Message] == true {
+			return nil
+		}
+
 		messages = append(messages, body.Message)
+		messagesMap[body.Message] = true
+
+		neighbourNodes := topology[msg.Dest]
+
+		for _, node := range neighbourNodes {
+			n.Send(node, Json{
+				"message": body.Message,
+				"type":    "broadcast",
+			})
+		}
+
+		if broadcastReceivedFromNeighbour(msg) {
+			return nil
+		}
 
 		return n.Reply(msg, Json{
 			"type": "broadcast_ok",
@@ -38,6 +60,13 @@ func main() {
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body Body
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		topology = body.Topology
+
 		return n.Reply(msg, Json{
 			"type": "topology_ok",
 		})
@@ -46,4 +75,8 @@ func main() {
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func broadcastReceivedFromNeighbour(msg maelstrom.Message) bool {
+	return strings.Contains(msg.Src, "n")
 }
--- readme.md
+++ readme.md
@@ -3,10 +3,13 @@
 A series of distributed systems challenges. By [fly.io](https://fly.io/dist-sys/)
 
 ## [Challenge #1: Echo](https://fly.io/dist-sys/1/)
-`echo`
+Run `echo` configuration
 
 ## [Challenge #2: Unique ID Generation](https://fly.io/dist-sys/2/)
-`unique-ids`
+Run `unique-ids` configuration
 
 ## [Challenge #3a: Single-Node Broadcast](https://fly.io/dist-sys/3a/)
-`broadcast-3a`
+Run `broadcast-3a` configuration
+
+## [Challenge #3b: Multi-Node Broadcast](https://fly.io/dist-sys/3b/)
+Run `broadcast-3b` configuration
